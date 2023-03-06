99cc7dfb877fb585074af14164e8b58f5b0e0f18
Marc Gavilan <marcgavilangil@gmail.com>
2023-03-06 18:27:13 +0100

    solve: crackme.py

---
+++ crackme-py/solution.sh
@@ -0,0 +1,5 @@
+# The solution to this lies in the use of the vulnerable input() function in Python2.7
+# If someone knows a way to crack this WITHOUT CHANGING THE SOURCE CODE in the latest
+# version of Python let me know
+
+echo decode_secret(bezos_cc_secret) | python2.7 crackme.py 
